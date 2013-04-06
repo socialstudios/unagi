f63f7b83968039221636faf547500abd5e759fbc
Ori Shmolovsky <ori@musetrack.com>
2013-04-06 17:50:05 +0300

    implemented shows list

--- app/controllers/show.rb
+++ app/controllers/show.rb
@@ -1,33 +1,19 @@
 Unagi::App.controllers :show do
+  get :index, :with => :id do
+    @show_id = params[:id];
+    show = Show.find_by(id: params[:id])
+    @title = show.title
+    haml 'show/show'.to_sym, :layout => 'layouts/show'
+  end
+
+  get 'data/:id.json', :provides => [:json] do
+     show = Show.find_by(id: params[:id])
+     show = JSON.parse(show.data)
+     show.to_json
+  end
+
   get :index do
+    @title = 'Shows'
     haml 'show/index'.to_sym, :layout => 'layouts/show'
   end
-
-  get ':id.json', :provides => [:json] do
-    show = Show.find_by(id: params[:id])
-    show['lineup'] = JSON.parse(show.data)
-    show.data = nil
-    show.to_json
-  end
-
-  get ':id', :provides => [:html] do
-    "Hello #{Show.find_by(id: params[:id]).to_json}"
-  end
-
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-
-
 end
--- public/javascripts/loadVideos.js
+++ public/javascripts/loadVideos.js
@@ -1,4 +1,5 @@
-$.getJSON('/videos.json', function(data){
+
+$.getJSON('http://localhost:3000/show/data/' + showId + '.json', function(data){
   Videos.setVideos(data.videos);
   dispatcher.trigger('videos_loaded');
 }).fail(function() {console.log('failed fetching videos')});
