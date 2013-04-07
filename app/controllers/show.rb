7118deea38dc34d36bd468764caaedb15285b4f6
Ori Shmolovsky <ori@musetrack.com>
2013-04-07 16:00:25 +0300

    added next show, not working

--- app/controllers/show.rb
+++ app/controllers/show.rb
@@ -1,8 +1,23 @@
 Unagi::App.controllers :show do
   get :index, :with => :id do
-    @show_id = params[:id];
-    show = Show.find_by(id: params[:id])
-    @title = show.title
+    @show_id = params[:id]
+
+    '''
+    @next_show = nil
+    found = nil
+    Show.each do |show|
+      if found then
+        @next_show = show.id
+        break
+      end
+      if @show_id == show.id then
+        found = true
+      end
+    end
+    '''
+
+    show_obj = Show.find_by(id: params[:id])
+    @title = show_obj.title
     haml 'show/show'.to_sym, :layout => 'layouts/show'
   end
 
