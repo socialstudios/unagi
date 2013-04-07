8e8a3a13d3dd35918279db7493575da2e2535cf1
Ori Shmolovsky <ori@musetrack.com>
2013-04-08 01:38:21 +0300

    added create form, improved design of show list and show, added fb comments, separted javascript assets groups

--- app/controllers/show.rb
+++ app/controllers/show.rb
@@ -1,4 +1,35 @@
 Unagi::App.controllers :show do
+  get '/create' do
+    haml 'show/create'.to_sym
+  end
+
+  post '/create' do
+    # prepare post parameters for the db
+    videos = Array.new
+    videos.push params[:show]['opening']
+    videos.push params[:show]['thevideo']
+    videos.push params[:show]['closing']
+    # isolate youtube id from the url
+    for i in 0..2
+      videos[i] = videos[i].split('=')[1]
+    end
+    prepared_data = {}
+    prepared_data['data'] = {}
+    prepared_data['title'] = 'dontcare'
+    prepared_data['data']['videos'] = videos
+    prepared_data['data']['topic'] = params[:show]['topic']
+    prepared_data['data']['name'] = params[:show]['name']
+    prepared_data['data']['email'] = params[:show]['email']
+    prepared_data['data'] = prepared_data['data'].to_json
+    # save to db
+    @show = Show.new(prepared_data)
+    if @show.save then
+      "Thanks, your data has been saved."
+    else
+      "Something went wrong, click back and try again."
+    end
+  end
+
   get :index, :with => :id do
     @show_id = params[:id];
     show = Show.find_by(id: params[:id])
