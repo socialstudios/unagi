23f464efb5061cde2e45311fea8ba419041303c2
Ran Tavory <rantav@gmail.com>
2013-04-08 23:49:37 +0300

    Move the database configuration to config/database.rb. See if this works on heroku

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,14 @@
-# database_name = case Padrino.env
-#   when :development then 'unagi_development'
-#   when :production  then 'unagi_production'
-#   when :test        then 'unagi_test'
-# end
+database_name = case Padrino.env
+  when :development then 'unagi_development'
+  # when :production  then 'unagi_production'
+  when :test        then 'unagi_test'
+end
 
-# # Use MONGOLAB_URI if it's set as an environmental variable
-# Mongoid::Config.sessions = case Padrino.env
-#   when :development then {default: {hosts: ["localhost:27017"], database: database_name}}
-#   when :production  then {default: {uri: ENV['MONGOLAB_URI'] }}
-#   when :test        then {default: {hosts: ["localhost:27017"], database: database_name}}
-# end
+# Use MONGOLAB_URI if it's set as an environmental variable
+Mongoid::Config.sessions = case Padrino.env
+  when :development then {default: {hosts: ["localhost:27017"], database: database_name}}
+  when :production  then {default: {uri: ENV['MONGOLAB_URI'] }}
+  when :test        then {default: {hosts: ["localhost:27017"], database: database_name}}
+end
 
-Mongoid.load!(File.join(Padrino.root, 'config', 'database.yml'), Padrino.env)
+# Mongoid.load!(File.join(Padrino.root, 'config', 'database.yml'), Padrino.env)
